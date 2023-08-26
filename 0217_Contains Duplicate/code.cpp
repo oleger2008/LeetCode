5e437b15cedd715d5416346cf1651bb3083d9fdb
oleger2008 <otsoy1995@yandex.ru>
2023-08-26 22:37:26 +0300

    refresh task #0217

--- 0217_Contains Duplicate/code.cpp
+++ 0217_Contains Duplicate/code.cpp
@@ -1,13 +1,35 @@
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// dict: [1, 2, 3]
+// arr [1,2,3,1]
+// anw: true
+
+// arr.size: [1..10^5]
+// value: [-10^9..10^9]
+
+// time: O(n)
+// memory: O(n)
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::unordered_set<int> unique_nums;
-        unique_nums.reserve(nums.size());
-        for (const auto num : nums) {
-            if (unique_nums.count(num)) {
+        unordered_set<int> collection;
+        collection.reserve(nums.size());
+        for (const auto n : nums) {
+            if (collection.count(n)) {
                 return true;
             }
-            unique_nums.insert(num);
+            collection.insert(n);
         }
         return false;
     }
