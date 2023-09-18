509e64df0442e1d2d5a211e17ea43fc2fe7ccf33
oleger2008 <otsoy1995@yandex.ru>
2023-09-19 01:18:25 +0300

    resolve task #0876

--- 0876_Middle of the Linked List/code.cpp
+++ 0876_Middle of the Linked List/code.cpp
@@ -1,3 +1,51 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+
+// Example 2:
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+// list.size: [1..100]
+// node.val: [1..100]
+
+// s
+// f
+// 1 -> 2 -> 3 -> 4 -> 5 -> null
+
+//      s
+//           f
+// 1 -> 2 -> 3 -> 4 -> 5 -> null
+
+//           s
+//                     f
+// 1 -> 2 -> 3 -> 4 -> 5 -> null
+// ans: s = 3
+
+// ------------
+
+// s
+// f
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6-> null
+
+//      s
+//           f
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6-> null
+
+//           s
+//                     f
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6-> null
+
+//                s
+//                              f
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6-> null
+// ans: s = 4
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,33 +56,18 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution { // time: O(N), mem: O(1)
 public:
-    // naive
-    // ListNode* middleNode(ListNode* head) {
-    //     size_t size = 0u;
-    //     auto *node = head;
-    //     while (node != nullptr) {
-    //         node = node->next;
-    //         ++size;
-    //     }
-    //     size = size / 2;
-    //     for (size_t i = 0u; i < size; ++i) {
-    //         head = head->next;
-    //     }
-    //     return head;
-    // }
-    
-    // fast slow ptr
-    ListNode *middleNode(ListNode *const head) {
-        auto *slow = head;
-        auto *fast = head;
-        
-        while ((fast != nullptr) && (fast->next != nullptr)) {
-            slow = slow->next;
-            fast = fast->next->next;
+    ListNode* middleNode(ListNode* head) {
+        //                s
+        //                                f
+        // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+        auto *slow = head; // slow = 1
+        auto *fast = head; // fast = 1
+        while (fast && fast->next) { // null != null && _ != null
+            slow = slow->next; // slow = 4
+            fast = fast->next->next; // fast = null
         }
-        
         return slow;
     }
 };
